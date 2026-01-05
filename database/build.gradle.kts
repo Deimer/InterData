7fac598b07b946ad7803f8523e22afd2cfb733b4
deimer215@gmail.com <deimer215@gmail.com>
2026-01-04 22:55:27 -0500

    Configured 'database' module with Room dependencies

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
 }
+
+val databaseDebug = providers.gradleProperty("DATABASE_DEBUG").get()
+val databaseRelease = providers.gradleProperty("DATABASE_RELEASE").get()
 
 android {
     namespace = "com.testdeymervilla.database"
@@ -15,11 +19,24 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
+            )
+            buildConfigField(
+                "String", "DATABASE_NAME", databaseDebug
+            )
+        }
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField(
+                "String", "DATABASE_NAME", databaseRelease
             )
         }
     }
@@ -30,13 +47,32 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
+    //Kotlin core
     implementation(libs.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    //Room database
+    api(libs.room.ktx)
+    ksp(libs.room.compiler)
+    //Hilt
+    api(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    //Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.androidx.core)
+    androidTestImplementation(libs.androidx.core.ktx)
+    androidTestImplementation(libs.androidx.archCore.testing)
+    //Robolectric
+    testImplementation(libs.robokectric.test)
+    //Mockito
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.mockito.inline)
+    testImplementation(libs.coroutines.test)
 }
